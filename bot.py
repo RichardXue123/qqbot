d78e412656fd0da72256b44aee260cea9fa176f3
Zhenxuan Su <zx.sine.su@gmail.com>
2024-07-26 10:34:46 +0800

    add consoleAdapter to enable local test

--- bot.py
+++ bot.py
@@ -1,12 +1,16 @@
 import nonebot
-from nonebot.adapters.onebot.v11 import Adapter
+from nonebot.adapters.onebot.v11 import Adapter as V11Adapter
+from nonebot.adapters.onebot.v12 import Adapter as V12Adapter
+from nonebot.adapters.console import Adapter as ConsoleAdapter
 
 # 初始化 NoneBot
 nonebot.init()
 
 # 注册适配器
 driver = nonebot.get_driver()
-driver.register_adapter(Adapter)
+driver.register_adapter(V11Adapter)
+driver.register_adapter(V12Adapter)
+driver.register_adapter(ConsoleAdapter)
 
 # 在这里加载插件
 nonebot.load_builtin_plugins("echo")  # 内置插件
